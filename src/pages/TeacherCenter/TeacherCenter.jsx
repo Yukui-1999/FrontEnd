2a5abe44f1bb2eb2776c9e4cedd51b7c0ef0e87a
Yukui-1999 <80956832+Yukui-1999@users.noreply.github.com>
2021-06-27 13:50:02 +0800

    Update TeacherCenter.jsx

--- src/pages/TeacherCenter/TeacherCenter.jsx
+++ src/pages/TeacherCenter/TeacherCenter.jsx
@@ -9,7 +9,16 @@ import MyClass from './MyClass'
 import FindCourse from './FindCourse'
 import ChangeTeaInfo from './ChangeTeaInfo'
 import TeaChangePSW from './TeaChangePSW'
-
+import CourseResource from './CourseResource'
+import AddHW from './AddHW'
+import HWMarking from './HWMarking'
+import PersonalResource from './PersonalResource'
+import Quiz from './Quiz'
+import GradeAnalysis from './GradeAnalysis'
+import Quizanalysis from './Quizanalysis'
+import HWanalysis from './HWanalysis'
+import Analysischarts from './Analysischarts'
+import Studentanalysis from './Studentanalysis'
 import {
   BarChartOutlined,
   FileTextOutlined,
@@ -77,9 +86,7 @@ class TeacherCenter extends React.Component {
               成绩分析
               <Link to={{ pathname: '/TeacherCenter/GradeAnalysis', state: { username: this.state.username, psw: this.state.psw } }}></Link>
           </Menu.Item>
-            <Menu.Item key="5" icon={<BookOutlined />}>
-              作业发布
-          </Menu.Item>
+           
             <Menu.Item key="6" icon={<TeamOutlined />}>
               相关申请
               <Link to={{ pathname: '/TeacherCenter/TApplication', state: { username: this.state.username, psw: this.state.psw } }}></Link>
@@ -112,7 +119,16 @@ class TeacherCenter extends React.Component {
                 <Route path="/TeacherCenter/FindCourse" component={FindCourse}></Route>
                 <Route path="/TeacherCenter/TeaChangePSW" component={TeaChangePSW}></Route>
                 <Route path="/TeacherCenter/ChangeTeaInfo" component={ChangeTeaInfo}></Route>
-
+                <Route path="/TeacherCenter/CourseResource" component={CourseResource}></Route>
+                <Route path="/TeacherCenter/AddHW" component={AddHW}></Route>
+                <Route path="/TeacherCenter/HWMarking" component={HWMarking}></Route>
+                <Route path="/TeacherCenter/PersonalResource" component={PersonalResource}></Route>
+                <Route path="/TeacherCenter/GradeAnalysis" component={GradeAnalysis}></Route>
+                <Route path="/TeacherCenter/Quiz" component={Quiz}></Route>
+                <Route path="/TeacherCenter/Quizanalysis" component={Quizanalysis}></Route>
+                <Route path="/TeacherCenter/HWanalysis" component={HWanalysis}></Route>
+                <Route path="/TeacherCenter/Studentanalysis" component={Studentanalysis}></Route>
+                <Route path="/TeacherCenter/Analysischarts" component={Analysischarts}></Route>
                 <Redirect to="/TeacherCenter/TeacherInfo"></Redirect>
               </Switch>
             </div>
